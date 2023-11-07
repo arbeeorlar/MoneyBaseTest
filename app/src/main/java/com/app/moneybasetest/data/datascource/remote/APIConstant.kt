b6f713e362b3be5f8c68b5a954401d615e166362
a6336 <biola.gold@gmail.com>
2023-11-07 10:33:00 +0100

    money base test

--- app/src/main/java/com/app/moneybasetest/data/datascource/remote/APIConstant.kt
+++ app/src/main/java/com/app/moneybasetest/data/datascource/remote/APIConstant.kt
@@ -5,7 +5,7 @@ class APIConstant {
 
     //Base URL
     companion object {
-        val API_KEY =  "e3302a9b04mshf73c8db18170036p1c04b9jsn07ccebc4f1f3" // "fc6fe56158mshc8a6580927bc237p10762cjsna35d351cb2fa"
+        val API_KEY = "fc6fe56158mshc8a6580927bc237p10762cjsna35d351cb2fa" // "e3302a9b04mshf73c8db18170036p1c04b9jsn07ccebc4f1f3" //
         val BASE_URL = "https://yh-finance.p.rapidapi.com/"
         val API_HOST = "yh-finance.p.rapidapi.com"
     }
